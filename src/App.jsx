98ca7935b4eeeebd63e011c4701bcca2e4a3432f
Pearl Phahlane <pearlulumotaung@gmail.com>
2024-11-11 14:08:28 +0200

    populate the form with the classnames and types, placeholders and add text to the button

--- src/App.jsx
+++ src/App.jsx
@@ -17,10 +17,10 @@ function App() {
 
       {/*Form with two input fields and a button*/}
       <main>
-        <form>
-          <input></input>
-          <input></input>
-          <button></button>
+        <form className="form">
+          <input type="text" placeholder="Top text" className="form--input"></input>
+          <input type="text" placeholder="Bottom text" className="form--input"></input>
+          <button className="form--button"> Get a new meme image 🖼</button>
         </form>
       </main>
     </>
