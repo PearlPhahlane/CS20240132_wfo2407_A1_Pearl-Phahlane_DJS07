3802fa9cae17cf15b1560973fc53b5dc35c6d4f2
Pearl Phahlane <pearlulumotaung@gmail.com>
2024-11-11 13:40:13 +0200

    add header with header title and project name

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,16 @@ function App() {
 
   return (
     <>
-     
+      <header className="header">
+        <img 
+            src=""
+            className="header--image"
+        />
+        <h2 className="header--title">Meme Generator</h2>
+        <h4 className="header--project">React Course - Project 3</h4>
+      </header> 
     </>
-  )
+  );
 }
 
 export default App
