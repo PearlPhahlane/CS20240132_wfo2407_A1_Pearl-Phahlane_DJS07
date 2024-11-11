bac7f903066461825b5a0ca820c24f96369d82ac
Pearl Phahlane <pearlulumotaung@gmail.com>
2024-11-11 13:53:38 +0200

    add troll face image the correct way by importing images and using curly braces to add the import name so the image can be displayed, also add import react to clear error messages

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import React from 'react'; //had to import to remove errors in vs code
+import trollFace from "./images/troll-face.png"; //had to import with a descriptive name
 
 function App() {
 
@@ -5,8 +7,9 @@ function App() {
     <>
       <header className="header">
           <img 
-              src="./images/troll-face.png"
+              src= {trollFace} //had to use the descriptive name from the import for image to show
               className="header--image"
+              alt= "Meme Generator Logo"
           />
           <h2 className="header--title">Meme Generator</h2>
           <h4 className="header--project">React Course - Project 3</h4>
